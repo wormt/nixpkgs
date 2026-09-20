a9a2debd7af6fa23ae38b155060e128e4ed2c2c9
wormt <209373679+wormt@users.noreply.github.com>
2026-09-20 14:13:10 -0700

    pulumiverse-talos: 0.7.1 -> 0.8.1

--- pkgs/by-name/pu/pulumi/plugins/pulumiverse-talos/package.nix
+++ pkgs/by-name/pu/pulumi/plugins/pulumiverse-talos/package.nix
@@ -5,10 +5,10 @@
 mkPulumiPackage rec {
   owner = "pulumiverse";
   repo = "pulumi-talos";
-  version = "0.7.1";
+  version = "0.8.1";
   rev = "v${version}"; # TODO: add support for `tag`
-  hash = "sha256-mk56p7vle61NdRKEaC8v0Eh9aJiilwdaDMwVvLaVRIM=";
-  vendorHash = "sha256-1xAIb7ZSsTqlVBpgFHc3RdQkNDngDT7A6tu7gEiGKjY=";
+  hash = "sha256-iioZkVD7pCt5DXyeT1qmSrwUDMq6hZT9jrTl0GTRM9Q=";
+  vendorHash = "sha256-RTHCFra/mUsJnKVGSEcUrSI8PSAdpXkm3bPifd6mNyA=";
   cmdGen = "pulumi-tfgen-talos";
   cmdRes = "pulumi-resource-talos";
   extraLdflags = [
